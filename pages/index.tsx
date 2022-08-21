d69a7eaaf4010a9965119fd2607722a94433c787
NoNejNeinNonNyet <nenadtodorovski1@gmail.com>
2022-08-21 09:49:50 +0200

    - add key for link items

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {links.map(({ id, title, description, category, imageUrl, url }) => (
             <LinkCard
+              key={id}
               id={id}
               title={title}
               description={description}
