f3b58add0360799b3eeb7984e91e1797afa52bec
NoNejNeinNonNyet <nenadtodorovski1@gmail.com>
2022-08-21 09:51:49 +0200

    - define User and Link types    - fields and relations

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,1 +1,26 @@
-export type Link = {};
+import { objectType } from "nexus";
+import { User } from "./User";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.string("id");
+    t.string("title");
+    t.string("description");
+    t.string("category");
+    t.string("imageUrl");
+    t.string("url");
+    t.list.field("users", {
+      type: User,
+      async resolve(parent, _args, ctx) {
+        return await ctx.prisma.link
+          .findUnique({
+            where: {
+              id: parent.id,
+            },
+          })
+          .users();
+      },
+    });
+  },
+});
