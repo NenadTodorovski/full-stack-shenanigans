ef50ccfc160e87ad35f24cc4047a428e90a8c331
NoNejNeinNonNyet <nenadtodorovski1@gmail.com>
2022-11-27 09:26:29 +0100

    Initial pagination logic    - cursors and all of the good stuff for better and optimized db querying

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { extendType, intArg, objectType, stringArg } from "nexus";
 import { User } from "./User";
 
 export const Link = objectType({
@@ -25,13 +25,90 @@ export const Link = objectType({
   },
 });
 
+export const Edge = objectType({
+  name: "Edge",
+  definition(t) {
+    t.string("cursor");
+    t.field("node", {
+      type: Link,
+    });
+  },
+});
+
+export const PageInfo = objectType({
+  name: "PageInfo",
+  definition(t) {
+    t.string("endCursor");
+    t.boolean("hasNextPage");
+  },
+});
+
+export const Response = objectType({
+  name: "Response",
+  definition(t) {
+    t.field("pageInfo", { type: PageInfo });
+    t.list.field("edges", {
+      type: Edge,
+    });
+  },
+});
+
 export const LinksQuery = extendType({
   type: "Query",
   definition(t) {
-    t.nonNull.list.field("links", {
-      type: "Link",
-      async resolve(_parent, _args, ctx) {
-        return await ctx.prisma.link.findMany();
+    t.field("links", {
+      type: "Response",
+      args: {
+        size: intArg(),
+        after: stringArg(),
+      },
+      async resolve(
+        _parent,
+        args: { size: number; after: string },
+        ctx
+      ): Promise<any> {
+        let queryResult;
+
+        if (!!args.after) {
+          queryResult = await ctx.prisma.link.findMany({
+            take: args.size,
+            skip: 1,
+            cursor: {
+              id: args.after,
+            },
+          });
+        } else {
+          queryResult = await ctx.prisma.link.findMany({ take: args.size });
+        }
+
+        if (queryResult.length > 0) {
+          const activeCursor = queryResult[queryResult.length - 1].id;
+          const nextQueryResult = await ctx.prisma.link.findMany({
+            take: args.size,
+            cursor: { id: activeCursor },
+          });
+
+          const result = {
+            pageInfo: {
+              endCursor: activeCursor,
+              hasNextPage: nextQueryResult.length >= args.size,
+            },
+            edges: queryResult.map((link) => ({
+              cursor: link.id,
+              node: link,
+            })),
+          };
+
+          return result;
+        }
+
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
+        };
       },
     });
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,35 @@ import { LinkCard } from "../components/LinkCard";
 import { gql, useQuery } from "@apollo/client";
 
 const GetAllLinksQuery = gql`
-  query {
-    links {
-      id
-      title
-      url
-      description
-      imageUrl
-      category
+  query Links($size: Int, $after: String) {
+    links(size: $size, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          title
+          description
+          category
+          imageUrl
+          url
+        }
+      }
     }
   }
 `;
 
 export default function Home() {
-  const { data: { links } = {}, loading, error } = useQuery(GetAllLinksQuery);
+  const {
+    data: { links } = {},
+    loading,
+    error,
+  } = useQuery(GetAllLinksQuery, {
+    variables: { size: 10 },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something went terribly wrong! {error.message}</p>;
@@ -30,17 +45,19 @@ export default function Home() {
 
       <div className="container mx-auto max-w-5xl my-20">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {links.map(({ id, title, description, category, imageUrl, url }) => (
-            <LinkCard
-              key={id}
-              id={id}
-              title={title}
-              description={description}
-              category={category}
-              imageUrl={imageUrl}
-              url={url}
-            />
-          ))}
+          {links.edges.map(
+            ({ node: { id, title, description, category, imageUrl, url } }) => (
+              <LinkCard
+                key={id}
+                id={id}
+                title={title}
+                description={description}
+                category={category}
+                imageUrl={imageUrl}
+                url={url}
+              />
+            )
+          )}
         </ul>
       </div>
     </div>
